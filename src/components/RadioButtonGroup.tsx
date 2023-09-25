ec9a360811dfa513c5f71b8944101732b344573e
morelmiles <morelmiles@gmail.com>
2023-09-25 16:25:55 +0300

    Add radio button group logic
    Signed-off-by: morelmiles <morelmiles@gmail.com>

--- src/components/RadioButtonGroup.tsx
+++ src/components/RadioButtonGroup.tsx
@@ -1,9 +1,58 @@
+import { FormControlLabel, Radio, RadioGroup } from '@mui/material';
 import * as React from 'react';
+import { useState } from 'react';
+
+import { useTranslation } from '../i18n/client';
+import theme from '../styles/theme';
 
 interface Props {}
 
 const RadioButtonGroup: React.FC<Props> = ({}) => {
-  return <></>;
+  const { t } = useTranslation();
+
+  const DIESEL = 'stepper.diesel';
+  const GPL = 'stepper.gpl';
+  const ELECTRIC = 'stepper.electric';
+  const BIKE_FOOD = 'stepper.bike-food';
+
+  const fuelOptions = [
+    { id: 1, title: t(DIESEL) },
+    { id: 2, title: t(GPL) },
+    { id: 3, title: t(ELECTRIC) },
+    { id: 4, title: t(BIKE_FOOD) },
+  ];
+
+  const [selectedValue, setSelectedValue] = useState(1);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSelectedValue(Number(event.target.value));
+  };
+
+  return (
+    <RadioGroup row className='flex justify-between'>
+      {fuelOptions.map((fuelOption: any) => (
+        <FormControlLabel
+          className='capitalize'
+          required
+          value='fuel'
+          key={fuelOption.id}
+          control={
+            <Radio
+              sx={{
+                '&.Mui-checked': {
+                  color: theme.colors.primary,
+                },
+              }}
+              checked={selectedValue === fuelOption.id}
+              onChange={handleChange}
+              value={fuelOption.id}
+            />
+          }
+          label={fuelOption.title}
+        />
+      ))}
+    </RadioGroup>
+  );
 };
 
 export default RadioButtonGroup;
