b78e84dfc8ea8d5d38d7c06ed47215354ef8c2cf
morelmiles <morelmiles@gmail.com>
2023-09-25 16:25:30 +0300

    add range slider logic
    Signed-off-by: morelmiles <morelmiles@gmail.com>

--- src/components/RangeSlider.tsx
+++ src/components/RangeSlider.tsx
@@ -1,9 +1,36 @@
+import Slider from '@mui/material/Slider';
 import * as React from 'react';
 
-interface Props {}
+import theme from '../styles/theme';
 
-const RangeSlider: React.FC<Props> = ({}) => {
-  return <></>;
+interface Props {
+  max: number;
+  marks: Array<{ value: number; label: string }>;
+}
+
+const RangeSlider: React.FC<Props> = ({ max, marks }) => {
+  return (
+    <Slider
+      defaultValue={0}
+      max={max}
+      sx={{
+        '& .MuiSlider-thumb': {
+          backgroundColor: theme.colors.default,
+        },
+        color: theme.colors.primary,
+        height: 8,
+        '& .MuiSlider-track': {
+          border: 'none',
+        },
+        '& .MuiSlider-markLabel': {
+          fontWeight: 600,
+        },
+      }}
+      step={1}
+      marks={marks}
+      min={0}
+    />
+  );
 };
 
 export default RangeSlider;
