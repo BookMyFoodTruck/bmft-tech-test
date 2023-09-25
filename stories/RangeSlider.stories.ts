31a89c1b618d4190010fc5d6572203c2888ff7ae
morelmiles <morelmiles@gmail.com>
2023-09-25 16:43:16 +0300

    chore: add storybook story for the range slider
    Signed-off-by: morelmiles <morelmiles@gmail.com>

---
+++ stories/RangeSlider.stories.ts
@@ -0,0 +1,48 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import RangeSlider from '../src/components/RangeSlider';
+
+const meta: Meta<typeof RangeSlider> = {
+  title: 'RangeSlide',
+  component: RangeSlider,
+  tags: ['autodocs'],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof RangeSlider>;
+
+export const SocketsRequired: Story = {
+  args: {
+    max: 8,
+    marks: [
+      {
+        value: 0,
+        label: '0',
+      },
+      {
+        value: 8,
+        label: '8',
+      },
+    ],
+  },
+};
+
+export const CatchmentArea: Story = {
+  args: {
+    max: 100,
+    marks: [
+      {
+        value: 0,
+        label: '0 KM',
+      },
+      {
+        value: 24,
+        label: '24 KM',
+      },
+      {
+        value: 100,
+        label: '+100 KM ',
+      },
+    ],
+  },
+};
